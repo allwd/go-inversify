889da39ee08743bf90c6619dd42696915efe0ada
Sebastian Ślączka <sebastian.slaczka@synergycodes.com>
2019-08-13 11:31:11 +0200

    visual adjustments

--- app/components/Actions.tsx
+++ app/components/Actions.tsx
@@ -1,19 +1,30 @@
 import * as React from 'react'
 
-function Actions({add, change, save, current, models}) {
+function Actions({ add, change, save, current, models }) {
     return (
         <div className={'actions'}>
             {models && Object.keys(models).map(key => (
                 <button
+                    className={'button'}
                     key={key}
                     onClick={() => change(key)}
-                    style={{fontWeight: String(current) === key ? 'bold' : 'unset'}}
+                    style={{ fontWeight: String(current) === key ? 'bold' : 'unset' }}
                 >
                     Model {key}
                 </button>
             ))}
-            <button onClick={add}>New Model</button>
-            <button onClick={save}>Save Diagram</button>
+            <button
+                className={'button'}
+                onClick={add}
+            >
+                New Model
+            </button>
+            <button
+                className={'button'}
+                onClick={save}
+            >
+                Save Diagram
+            </button>
         </div>
     )
 }
